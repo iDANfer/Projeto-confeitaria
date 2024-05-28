04b937a2036e44222a371d5f8a1ab46faa3d319e
Daniel Fernando <113464341+iDANfer@users.noreply.github.com>
2024-05-27 22:53:45 -0300

    Delete Projeto-Confeitaria directory

--- Projeto-Confeitaria/script.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector('.menu');
-const navMenu = document.querySelector('.nav-menu');
-
-menu.addEventListener('click', () => {
-    menu.classList.toggle('ativo');
-    navMenu.classList.toggle('ativo');
-});
